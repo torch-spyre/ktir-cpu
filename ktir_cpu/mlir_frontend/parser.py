8fbe5fb8d51d6f2970a9ea3b83857a0e15b44861
Yu Chin Fabian Lim <flim@sg.ibm.com>
2026-07-28 01:39:00 +0000

    fixes for mlir frontend adapt
    Signed-off-by: Yu Chin Fabian Lim <flim@sg.ibm.com>

--- ktir_cpu/mlir_frontend/parser.py
+++ ktir_cpu/mlir_frontend/parser.py
@@ -621,12 +621,13 @@ def _adapt_linalg_generic(mlir_op, attributes, result_type, operands):
     attributes["indexing_maps"] = maps
 
     # iterator_types: ArrayAttr of IteratorTypeAttr (parallel=0, reduction=1)
-    iter_types_attr = mlir_op.attributes.get("iteratorTypes")
+    iter_types_attr = mlir_op.attributes.get("iterator_types")
     if iter_types_attr is not None:
-        _ITER_TYPE_MAP = {0: "parallel", 1: "reduction"}
-        attributes["iterator_types"] = [
-            _ITER_TYPE_MAP.get(int(attr), str(attr)) for attr in iter_types_attr
-        ]
+        import re as _re
+        def _parse_iter_type(attr):
+            m = _re.search(r'iterator_type<(\w+)>', str(attr))
+            return m.group(1) if m else str(attr)
+        attributes["iterator_types"] = [_parse_iter_type(a) for a in iter_types_attr]
 
     # Block arguments are the bb0 names; stored in attributes for the executor fallback
     block = mlir_op.regions[0].blocks[0]
